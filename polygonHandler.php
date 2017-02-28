1b2c83fb1fd9cb21dc8d880a15c8a86f5bddb19a
dateutli <daviidalberto@gmail.com>
2017-02-28 10:21:56 -0700

    added mktFormatter function as well as new query processor

--- polygonHandler.php
+++ polygonHandler.php
@@ -1,11 +1,12 @@
 <?php
 	ini_set('memory_limit', '-1');
-	require('config.php');
-	$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-    if($conn->connect_error){
-        die('Connect Error (' . $conn->connect_errno . ') '
-        . $conn->connect_error);
-    }
+	// require('config.php');
+	$conn = mysqli_connect('ctis.utep.edu', 'ctis', '19691963', 'imsc');
+	// $conn = new mysqli('ctis.utep.edu', 'ctis', '19691963', 'imsc');
+    // if($conn->connect_error){
+    //     die('Connect Error (' . $conn->connect_errno . ') '
+    //     . $conn->connect_error);
+    // }
 	$toReturn = array();
 	if(isset($_GET['getMode']) AND $_GET['getMode'] == "polygons"){
 		//do somethig with this
@@ -14,23 +15,36 @@
 		$lat2 = $_GET['NE']['lat'];
 		$lat1 = $_GET['SW']['lat'];
 		$lng2 = $_GET['NE']['lng'];
-		$lng1 = $_GET['SW']['lng']; 
-		$sql = "CALL getBoundedCoordinates('$district', '$property', $lat1, $lat2, $lng1, $lng2);";
-		$result = $conn->multi_query($sql);
+		$lng1 = $_GET['SW']['lng'];
+		// $sql = "CALL getBoundedCoordinates('$district', '$property', $lat1, $lat2, $lng1, $lng2);";
+		$query = "SET @geom1 = 'POLYGON(($lng1	$lat1,$lng1	$lat2,$lng2	$lat2,$lng2	$lat1,$lng1	$lat1))'";
+		$result = mysqli_query($conn, $query);
+		$query = "SELECT OGR_FID, AsText(SHAPE) AS POLYGON, MUKEY FROM polygon WHERE st_intersects(ST_GeomFromText(@geom1,1), polygon.SHAPE)";
+		$result = mysqli_query($conn, $query);
+
+		$temp = array();
+		while($row = mysqli_fetch_assoc($result)){
+			$temp[] = $row;
+		}
+		// echo json_encode($temp);
+		// while($row = mysqli_fetch_assoc($result)){
+		// 	var_dump($row);
+		// }
+
 		$ids = array();
 		$coords= array();
-		do {
-	        /* store first result set */
-	        if ($result = $conn->store_result()) {
-	            while ($row = $result->fetch_row()) {
-	            	if(!in_array($row[0], $ids)){
-	            		$ids[$row[0]][] = array($row[1], $row[2]);
-	            	}
-	            }
-	            $result->free();
-	        }
-	    } while ($conn->next_result());
-		$toReturn['coords'] = $ids;
+		// do {
+	  //       /* store first result set */
+	  //       if ($result = $conn->store_result()) {
+	  //           while ($row = $result->fetch_row()) {
+	  //           	if(!in_array($row[0], $ids)){
+	  //           		$ids[$row[0]][] = array($row[1], $row[2]);
+	  //           	}
+	  //           }
+	  //           $result->free();
+	  //       }
+	  //   } while ($conn->next_result());
+		$toReturn['coords'] = $temp;
 	}
 	else if(isset($_GET['district'])){
 		$district = $_GET['district'];
@@ -39,11 +53,12 @@
 		if($result AND $result->num_rows < 400){
 			$toReturn['coords'] = $result->fetch_all();
 		}
-	} 
+	}
 	else if(isset($_POST['columns'])){
 		$sql = "SELECT * FROM attributes";
 		$result = $conn->query($sql);
-		$toReturn['columns'] = $result->fetch_all();
+		$toReturn['columns'] = array("0" => "plasticity", "1" => "plasticity");
+		// $toReturn['columns'] = $result->fetch_all();
 	}
 	header('Content-Type: application/json');
 	echo json_encode($toReturn);
